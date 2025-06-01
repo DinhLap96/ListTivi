f5287970bd55914d54fab4e5bd5368e9b5ba7baf
Administrator <dinhgaymuc@gmail.com>
2025-06-01 17:02:14 +0700

    tytty

--- ListTiVi/userscripts.js
+++ ListTiVi/userscripts.js
@@ -14,12 +14,8 @@
     meta.name = "viewport";
     meta.content = "width=3840, height=2160, initial-scale=1.0";
     document.head.appendChild(meta);
-    var el = document.documentElement
-                 , rfs = el.requestFullScreen
-                      || el.webkitRequestFullScreen
-                      || el.mozRequestFullScreen ;
-              rfs.call(el);
-            });
+    var el = document.documentElement , rfs = el.requestFullScreen|| el.webkitRequestFullScreen || el.mozRequestFullScreen ;
+    rfs.call(el);
   }
 })();
 /* End spoofViewport.js */
