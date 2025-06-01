0e6aa2f0074ca13efcead251a886f4cef7e0954f
Administrator <dinhgaymuc@gmail.com>
2025-06-01 16:15:51 +0700

    g

--- ListTiVi/userscripts.js
+++ ListTiVi/userscripts.js
@@ -3,18 +3,18 @@
 (function () {
   //if (window.screen.width >= 3840 || window.screen.height >= 2160) return;
 
-  var existing = document.querySelector('meta[name="viewport"]');
-  if (existing) {
-    existing.setAttribute(
-      "content",
-      "width=3840, height=2160, initial-scale=1.0"
-    );
-  } else {
-    var meta = document.createElement("meta");
-    meta.name = "viewport";
-    meta.content = "width=3840, height=2160, initial-scale=1.0";
-    document.head.appendChild(meta);
-  }
+//  var existing = document.querySelector('meta[name="viewport"]');
+//  if (existing) {
+//    existing.setAttribute(
+//      "content",
+//      "width=3840, height=2160, initial-scale=1.0"
+//    );
+//  } else {
+//    var meta = document.createElement("meta");
+//    meta.name = "viewport";
+//    meta.content = "width=3840, height=2160, initial-scale=1.0";
+//    document.head.appendChild(meta);
+//  }
 })();
 /* End spoofViewport.js */
 
