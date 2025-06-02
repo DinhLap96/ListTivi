2dc9a0a1e411d80d81e0992da7708846ab72ab43
Administrator <dinhgaymuc@gmail.com>
2025-06-02 15:29:16 +0700

    tfd

--- ListTiVi/userscripts.js
+++ ListTiVi/userscripts.js
@@ -7,7 +7,7 @@
   if (existing) {
     existing.setAttribute(
       "content",
-      "width=3840, height=2160, initial-scale=1.0"
+      "width=1920, height=1080, initial-scale=1.0"
     );
   } else {
     var meta = document.createElement("meta");
