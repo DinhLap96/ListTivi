401ad8abacdadcdc3010ff08ba61ea68ce3eab9f
Administrator <dinhgaymuc@gmail.com>
2025-06-02 20:17:41 +0700

    czc

--- ListTiVi/userscripts.js
+++ ListTiVi/userscripts.js
@@ -1,25 +1,25 @@
 /* Start spoofViewport.js */
 // Enables 4K resolution tricking youtube into thinking that we are on a 4K TV
 (function () {
-  //if (window.screen.width >= 3840 || window.screen.height >= 2160) return;
-//
-//  var existing = document.querySelector('meta[name="viewport"]');
-//  if (existing) {
-//    existing.setAttribute(
-//      "content",
-//      "width=3840, height=2160, initial-scale=1.0"
-//    );
-//  } else {
-//    var meta = document.createElement("meta");
-//    meta.name = "viewport";
-//    meta.content = "width=3840, height=2160, initial-scale=1.0";
-//    document.head.appendChild(meta);
-//  }
-//})();
-///* End spoofViewport.js */
-//
-///* Start menuTrigger.js */
-//// Add a "button" to fool you...
+  if (window.screen.width >= 3840 || window.screen.height >= 2160) return;
+
+  var existing = document.querySelector('meta[name="viewport"]');
+  if (existing) {
+    existing.setAttribute(
+      "content",
+      "width=3840, height=2160, initial-scale=1.0"
+    );
+  } else {
+    var meta = document.createElement("meta");
+    meta.name = "viewport";
+    meta.content = "width=3840, height=2160, initial-scale=1.0";
+    document.head.appendChild(meta);
+  }
+})();
+/* End spoofViewport.js */
+
+/* Start menuTrigger.js */
+// Add a "button" to fool you...
 //(function () {
 //  function getSearchBar() {
 //    const searchBars = document.querySelectorAll(
@@ -58,10 +58,10 @@
 //  }
 //
 //  addMenuButton();
-
-  // Here the fooling part begins.
-  // If the search tab is focused and the 'right arrow" is pressed, open up the menu.
-
+//
+//   Here the fooling part begins.
+//   If the search tab is focused and the 'right arrow" is pressed, open up the menu.
+//
 //  document.addEventListener("keydown", function (event) {
 //    if (event.key === "ArrowRight") {
 //      const searchBar = getSearchBar();
@@ -988,5 +988,5 @@
 //      true
 //    );
 //  }
-})();
+//})();
 /* End TizenTubeScripts.js */
