ca2dce9e7d5ec54d83aaa708b40959e0e7b625dc
Administrator <dinhgaymuc@gmail.com>
2025-06-02 14:54:46 +0700

    d

--- ListTiVi/userscripts.js
+++ ListTiVi/userscripts.js
@@ -1,7 +1,7 @@
 /* Start spoofViewport.js */
 // Enables 4K resolution tricking youtube into thinking that we are on a 4K TV
 (function () {
-  //if (window.screen.width >= 3840 || window.screen.height >= 2160) return;
+  if (window.screen.width >= 3840 || window.screen.height >= 2160) return;
 
   var existing = document.querySelector('meta[name="viewport"]');
   if (existing) {
