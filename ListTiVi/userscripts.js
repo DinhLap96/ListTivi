d29a2b5284cd8643bbdd86124112555814c3b4ea
Administrator <dinhgaymuc@gmail.com>
2025-06-02 15:31:54 +0700

    dsd

--- ListTiVi/userscripts.js
+++ ListTiVi/userscripts.js
@@ -2,12 +2,12 @@
 // Enables 4K resolution tricking youtube into thinking that we are on a 4K TV
 (function () {
   //if (window.screen.width >= 3840 || window.screen.height >= 2160) return;
-return;
+
   var existing = document.querySelector('meta[name="viewport"]');
   if (existing) {
     existing.setAttribute(
-      "content",
-      "width=1920, height=1080, initial-scale=1.0"
+      "content"
+//      "width=3840, height=2160, initial-scale=1.0"
     );
   } else {
     var meta = document.createElement("meta");
