a45ad471dacd587872534f0270ade0e32ad38a5e
Administrator <dinhgaymuc@gmail.com>
2025-06-07 12:43:22 +0700

    asewes

--- ListTiVi/userscripts.js
+++ ListTiVi/userscripts.js
@@ -1,15 +1,21 @@
-var elementsToRemove = document.querySelectorAll('tp-yt-paper-dialog[style-target="host"][role="dialog"][tabindex="-1"][style*="position: fixed; top: 242.25px; left: 270px;"]');
-for (var i = 0; i < elementsToRemove.length; i++) {
-
-elementsToRemove[i].remove();
-
-}
 
 
-var playButton = document.querySelector('button.ytp-play-button');
+(function () {
 
-if (playButton) {
+    var elementsToRemove = document.querySelectorAll('tp-yt-paper-dialog[style-target="host"][role="dialog"][tabindex="-1"][style*="position: fixed; top: 242.25px; left: 270px;"]');
+    for (var i = 0; i < elementsToRemove.length; i++) {
 
-playButton.click();
+    elementsToRemove[i].remove();
 
-}
+    }
+
+
+    var playButton = document.querySelector('button.ytp-play-button');
+
+    if (playButton) {
+
+    playButton.click();
+
+    }
+
+})();
