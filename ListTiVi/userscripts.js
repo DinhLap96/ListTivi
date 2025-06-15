a95f75a21eccf559891043403dd78be3ee1a8ed3
Administrator <dinhgaymuc@gmail.com>
2025-06-15 22:06:31 +0700

    szasa

--- ListTiVi/userscripts.js
+++ ListTiVi/userscripts.js
@@ -264,34 +264,6 @@
     }
   }
 
-  function customAction(action, parameters) {
-    switch (action) {
-      case "SETTINGS_UPDATE":
-        modernUI(true, parameters);
-        break;
-      case "SKIP":
-        const video = document.querySelector("video");
-        if (video) {
-          video.currentTime = parameters.time;
-        }
-        resolveCommand({
-          signalAction: {
-            signal: "POPUP_BACK",
-          },
-        });
-        break;
-    }
-  }
-
-  /**
-   * This is a minimal reimplementation of the following uBlock Origin rule:
-   * https://github.com/uBlockOrigin/uAssets/blob/3497eebd440f4871830b9b45af0afc406c6eb593/filters/filters.txt#L116
-   *
-   * This in turn calls the following snippet:
-   * https://github.com/gorhill/uBlock/blob/bfdc81e9e400f7b78b2abc97576c3d7bf3a11a0b/assets/resources/scriptlets.js#L365-L470
-   *
-   * Seems like for now dropping just the adPlacements is enough for YouTube TV
-   */
   const origParse = JSON.parse;
   JSON.parse = function () {
     const r = origParse.apply(this, arguments);
