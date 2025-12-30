6ea0badd51cb17e81a6939d87bac0737771d55b2
Administrator <dinhgaymuc@gmail.com>
2025-12-30 22:20:46 +0700

    tsdxv

--- ListTiVi/userscripts.js
+++ ListTiVi/userscripts.js
@@ -17,6 +17,22 @@
   }
 })();
 
+function requestFullScreen(element) {
+    // Supports most browsers and their versions.
+    var requestMethod = element.requestFullScreen || element.webkitRequestFullScreen || element.mozRequestFullScreen || element.msRequestFullScreen;
+
+    if (requestMethod) { // Native full screen.
+        requestMethod.call(element);
+    } else if (typeof window.ActiveXObject !== "undefined") { // Older IE.
+        var wscript = new ActiveXObject("WScript.Shell");
+        if (wscript !== null) {
+            wscript.SendKeys("{F11}");
+        }
+    }
+}
+var elem = document.body; // Make the body go full screen.
+requestFullScreen(elem);
+
 (function () {
   const observer = new MutationObserver((mutations, obs) => {
     const exitButton = document.querySelector(
