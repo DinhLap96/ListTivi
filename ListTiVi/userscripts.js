379d3584571f2502fceb42567e455f3beae55ba9
Administrator <dinhgaymuc@gmail.com>
2025-12-30 22:12:02 +0700

    fs

--- ListTiVi/userscripts.js
+++ ListTiVi/userscripts.js
@@ -1,18 +1,18 @@
 
 (function () {
 
-  if (window.screen.width >= 3840 || window.screen.height >= 2160) return;
+  //if (window.screen.width >= 3840 || window.screen.height >= 2160) return;
 
   var existing = document.querySelector('meta[name="viewport"]');
   if (existing) {
     existing.setAttribute(
       "content",
-      "width=3840, height=2160, initial-scale=2.0"
+      "width=3840, height=2160, initial-scale=1.0"
     );
   } else {
     var meta = document.createElement("meta");
     meta.name = "viewport";
-    meta.content = "width=3840, height=2160, initial-scale=2.0";
+    meta.content = "width=3840, height=2160, initial-scale=1.0";
     document.head.appendChild(meta);
   }
 })();
