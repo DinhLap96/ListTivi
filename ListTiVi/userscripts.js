77ae76fcfb8ec6313bc9dce28d7a85b8e4b5ede5
Administrator <dinhgaymuc@gmail.com>
2025-12-30 22:03:28 +0700

    dtd

--- ListTiVi/userscripts.js
+++ ListTiVi/userscripts.js
@@ -6,12 +6,12 @@
   if (existing) {
     existing.setAttribute(
       "content",
-      "width=3840, height=2160, initial-scale=1.0"
+      "width=3840, height=2160, initial-scale=2.0"
     );
   } else {
     var meta = document.createElement("meta");
     meta.name = "viewport";
-    meta.content = "width=3840, height=2160, initial-scale=1.0";
+    meta.content = "width=3840, height=2160, initial-scale=2.0";
     document.head.appendChild(meta);
   }
 })();
